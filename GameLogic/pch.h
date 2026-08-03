eeb4c86eaacf24f73cb8be85c50fdfeab0e86d73
Zwaliebaba <zwalie@live.com>
2026-08-03 21:04:50 +0200

    Build performance updates

--- GameLogic/pch.h
+++ GameLogic/pch.h
@@ -1,3 +1,19 @@
 #pragma once
 
 #include "NeuronClient.h"
+
+#include "Resource.h"
+#include "LanguageTable.h"
+#include "SoundSystem.h"
+
+#include "AppState.h"
+#include "DebugRender.h"
+#include "GameTime.h"
+#include "MathUtils.h"
+#include "Profiler.h"
+#include "Shape.h"
+#include "TextRenderer.h"
+#include "WorldPointers.h"
+
+#include "GlobalWorld.h"
+#include "Location.h"
